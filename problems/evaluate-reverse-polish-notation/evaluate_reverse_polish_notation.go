3cd66b575e5041581458f46f4c2f8087d0a0c661
Peng Zhang <pengubco@users.noreply.github.com>
2023-06-18 22:56:21 -0700

    Evaluate Reverse Polish Notation. Stack

---
+++ problems/evaluate-reverse-polish-notation/evaluate_reverse_polish_notation.go
@@ -0,0 +1,28 @@
+package evaluate_reverse_polish_notation
+
+import "strconv"
+
+func evalRPN(tokens []string) int {
+	var numbers []int
+	for _, s := range tokens {
+		l := len(numbers)
+		if s == "*" || s == "+" || s == "-" || s == "/" {
+			a, b := numbers[l-2], numbers[l-1]
+			numbers = numbers[:l-1]
+			switch s {
+			case "*":
+				numbers[l-2] = a * b
+			case "+":
+				numbers[l-2] = a + b
+			case "-":
+				numbers[l-2] = a - b
+			case "/":
+				numbers[l-2] = a / b
+			}
+			continue
+		}
+		a, _ := strconv.Atoi(s)
+		numbers = append(numbers, a)
+	}
+	return numbers[0]
+}
