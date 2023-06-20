20332010ac57dfbc44ac87eeef0ef290fd5a3b1f
Peng Zhang <pengubco@users.noreply.github.com>
2023-06-20 00:29:26 -0700

    wip.

---
+++ problems/h-index/h_index_test.go
@@ -0,0 +1,25 @@
+package h_index
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_hIndex(t *testing.T) {
+	cases := []struct {
+		citations []int
+		result    int
+	}{
+		{[]int{3, 0, 6, 1, 5}, 3},
+		{[]int{0, 0, 0}, 0},
+		{[]int{1}, 1},
+		{[]int{2}, 1},
+		{[]int{0, 10}, 1},
+	}
+
+	for _, d := range cases {
+		t.Run("", func(tt *testing.T) {
+			assert.Equal(tt, d.result, hIndex(d.citations))
+		})
+	}
+}
